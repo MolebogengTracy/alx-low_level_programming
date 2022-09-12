8a52c2ea14e81eda3fb72d65afa9bbcc77213f43
MolebogengTracy <tracytsima4@gmail.com>
2022-09-12 14:06:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,12 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/**
- * main- Entry point
- *
- * Return: Always success
- */
-int main (void)
-{
-	int n;
-	Srand(time(0));
-	n= rand()-RAND_MAX/2;
-	if(n>0)
-	  printf("%d positive\n",n);
 
-	else if(n==0)
-		printf("%d Zero\n",n);
-	else
-		printf("%d Negative\n",n);
+/**
+ * main-Printing
+ *
+ * Return Always (successs)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
