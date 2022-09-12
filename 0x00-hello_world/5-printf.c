de3e26a9c224f7c768aeb5503acd40609d5fb1ba
MolebogengTracy <tracytsima4@gmail.com>
2022-09-12 14:25:43 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#incliude <stdio.h>
+
+/**
+ * main-entry pint
+ *
+ * Return Always 0 (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
