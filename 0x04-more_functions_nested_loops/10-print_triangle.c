f1021b476c3689ddc0c99ff3c6e3f54156c6c01b
MolebogengTracy <tracytsima4@gmail.com>
2022-09-15 13:14:31 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,7 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (inc1 = 1; inc1 <=size; inc++)
+		for (inc1 = 1; inc1 <= size; inc++)
 		{
 			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
